06f6394327a52437171055da73222629582b8c11
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2020-10-20 17:57:43 -0600

    initialized models objects

---
+++ alexandria/models/__init__.py
@@ -0,0 +1,7 @@
+from .model import Model
+from .sklearnmodel import SklearnModel
+
+__all__ = [
+    'Model',
+    'SklearnModel'
+]
---
+++ alexandria/models/sklearnmodel.py
@@ -0,0 +1,5 @@
+from .model import Model
+
+class SklearnModel(Model):
+    def __init__(self):
+        print('SklearnModel constructor reached!')
