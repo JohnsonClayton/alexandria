bd20a8b5e3d4ef7d7ecf761a942419aa6cc436b5
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2020-10-22 14:27:10 -0600

    Added pandas functionality

--- alexandria/dataset/datasetmanager.py
+++ alexandria/dataset/datasetmanager.py
@@ -1,9 +1,12 @@
+import warnings
+
 from sklearn.utils import Bunch
+
+from pandas import DataFrame
 class DatasetManager:
-    def __init__(self, dataset=None, xlabels=[], ylabels=''):
+    def __init__(self, dataset=None, xlabels=[], ylabels='', target_type='', num_classes=None):
         # Initialize values
         self.target_type = None
-        self.datatype = None
 
         # Make sure that the provided labels are the correct data types (string or list of strings)
         # TO-DO: Would ints be valid as well?
@@ -26,14 +29,22 @@ class DatasetManager:
                 self.ylabels = ylabels
             else:
                 raise ValueError('ylabels argument must be string, not {}'.format( str( type( ylabels ) ) ))
-        
 
-        self.num_classes = None
-
-        if dataset:
+        # Figure out what datatype the dataset is
+        self.datatype = None
+        try:
+            if dataset:
+                self.datatype = type(dataset)
+        except ValueError:
+            # The dataset is defined as something, just not 'None'
             self.datatype = type(dataset)
+
+        # TO-DO: Is there any scrubbing we should do here? Seems a little sketchy...
         self.dataset = dataset
 
+        # Try to figure out what type of dataset this is and how many classes there are (if classification)
+        #   Note that these values can be overwritten by the user if our logic is wrong
+        self.num_classes = None
         if self.datatype == Bunch:
             if hasattr(self.dataset, 'target_names'):
                 self.target_type = 'classification'
@@ -43,7 +54,50 @@ class DatasetManager:
                 self.target_type = 'regression'
                 self.classes = None
                 self.num_classes = None
+        if self.datatype == DataFrame:
+            # Make sure all of the columns exist in the dataframe!
+            not_found = []
+            for xlabel in self.xlabels:
+                if xlabel not in self.dataset.columns:
+                    not_found.append(xlabel)
+            if self.ylabels not in self.dataset.columns:
+                not_found.append(self.ylabels)
+
+            if len(not_found) == 0:
+                # If the data series is an object, then it is classification type
+                if self.dataset[ self.ylabels ].dtype == 'O' or (self.dataset[ self.ylabels ].nunique() < 0.25*len(self.dataset[ self.ylabels ])):
+                    # The series is an object or there are enough unique values that it makes sense to not be regression
+                    self.target_type = 'classification'
+                    self.classes = list(self.dataset[ self.ylabels ].unique())
+                    self.num_classes = len(self.classes)
+                else:
+                # If there are a max of 4 samples per unique value, assume it is a regression dataset
+                    self.target_type = 'regression'
+                    self.classes = None
+                    self.num_classes = None
+            else:
+                # TO-DO: Figure out which column name does not exist and report it to the user
+                raise ValueError('These columns don\'t exist in the dataset: {}'.format(not_found))
 
         # Check to make sure provided dataset, xlabels, and ylabels all make sense together
 
+        # If the user defined these variables, then give them a heads up that this is kind of messed up compared 
+        #   to what we just calculated
+        if target_type and self.target_type != target_type:
+            if type(target_type) == str:
+                if target_type == 'regression' or target_type == 'classification':
+                    warnings.warn('User specified {} target type, but alexandria found {} target type. Assuming the user is correct...')
+                    self.target_type = target_type
+                else:
+                    raise ValueError('target_type argument must be \'regression\' or \'classification\', not {}'.format( target_type ))
+            else:
+                raise ValueError('target type must be of string type, not {}'.format( str( type(target_type) ) ))
+        
+        if num_classes and self.target_type == 'classification' and num_classes != self.num_classes:
+            if type(num_classes) == int:
+                warnings.warn('user specified {} classes, but alexandria found {} classes. Assuming user is correct...'.format(self.num_classes, num_classes))
+                self.num_classes = num_classes
+            else:
+                raise ValueError('num_classes argument must be integer, not {}'.format( str( type( num_classes ) ) ))
+            
         
--- alexandria/tests/datasetmanager_test.py
+++ alexandria/tests/datasetmanager_test.py
@@ -1,8 +1,9 @@
 import unittest
+from sklearn.datasets import load_iris, load_boston, load_diabetes
+
 
 from alexandria.dataset import DatasetManager
 
-from sklearn.datasets import load_iris, load_boston
 
 def fail(who):
     who.assertTrue( False )
@@ -34,12 +35,35 @@ class TestDatasetManager(unittest.TestCase):
         dm = DatasetManager(dataset=boston, xlabels=['data'], ylabels='target')
         self.assertEqual( dm.target_type, 'regression' )
         self.assertEqual( dm.dataset, boston )
-        self.assertEqual( dm.datatype, type(iris) )
+        self.assertEqual( dm.datatype, type(boston) )
         self.assertEqual( dm.xlabels, ['data'] )
         self.assertEqual( dm.ylabels, 'target' )
         self.assertEqual( dm.num_classes, None )
 
         # Check that it handles input data correctly - pandas.DataFrame
+        #   Classification dataset
+        iris = load_iris(as_frame=True)
+        iris = iris.frame
+        target_col = 'target'
+        data_cols = ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
+        dm = DatasetManager(dataset=iris, xlabels=data_cols, ylabels=target_col)
+        self.assertEqual( dm.target_type, 'classification' )
+        self.assertEqual( dm.datatype, type(iris) )
+        self.assertEqual( dm.xlabels, data_cols )
+        self.assertEqual( dm.ylabels, target_col )
+        self.assertEqual( dm.num_classes, 3 )
+
+        #  Regression dataset
+        diabetes = load_diabetes(as_frame=True)
+        diabetes = diabetes.frame
+        target_col = 'target'
+        data_cols = ['age', 'sex', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6']
+        dm = DatasetManager(dataset=diabetes, xlabels=data_cols, ylabels=target_col)
+        self.assertEqual( dm.target_type, 'regression' )
+        self.assertEqual( dm.datatype, type(diabetes) )
+        self.assertEqual( dm.xlabels, data_cols )
+        self.assertEqual( dm.ylabels, target_col )
+        self.assertEqual( dm.num_classes, None )
 
         # Fail if the xlabels or ylabels don't exist within the provided dataset
 
@@ -48,8 +72,6 @@ class TestDatasetManager(unittest.TestCase):
         # Fail if more than one ylabel is provided
 
         # Fail if datatypes for xlabel and ylabel are wrong
-
-        # 
 
 
 if __name__ == '__main__':
