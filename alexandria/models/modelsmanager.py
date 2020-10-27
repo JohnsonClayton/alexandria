6d86bbc52e0ab7e7b43f1908814f91f7443a05dd
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2020-10-27 12:03:38 -0600

    updated adding models tests

--- alexandria/models/modelsmanager.py
+++ alexandria/models/modelsmanager.py
@@ -1,39 +1,143 @@
-from alexandria.models import sklearn
+from alexandria.models import Model, sklearn
 
 import random
+import warnings
 class ModelsManager:
-    def __init__(self):
+    def __init__(self, default_library='scikit-learn'):
         self.models = {}
+
+        
+        # Set up dictionaries to allow for aliasing on library and model names
+        #   In this set up, if the key 'x' points to a string, then it is an alias.
+        #   If the provided key points to True, then the key is a legit library/model name
+        self.lib_aliases = {
+            'sklearn':'scikit-learn',
+            'scikit-learn': True
+        }
+        self.model_aliases = {
+            'rf': 'Random Forest',
+            'random forest': 'Random Forest',
+            'Random Forest': True,
+            'dt': 'Decision Tree',
+            'decision tree': 'Decision Tree',
+            'Decision Tree': True
+        }
 
         # Seed the random number generator, we just need random number for ids
         random.seed(0)
 
+        # Set the default library used to scikit-learn
+        if type(default_library) == str:
+            self.default_library = self.findLibraryNameWithAlias(default_library)
+        else:
+            raise ValueError('default library argument must be string type, not {}'.format( str( type(default_library) ) ))
+
+
     def getModelID(self):
         return int( 10000*random.random())
 
-    def addModel(self, model, lib='', args={}):
-        # Need to generate ID for the model and avoid repeats
-        model_id = self.getModelID()
-        while model_id in self.models:
-            model_id = self.getModelID()
+    def addModel(self, model, lib='', **args):
+        # Filter the lib and args values to account for aliases
+        #   Ex. sklearn -> scikit-learn
+        #   Ex. rf      -> Random Forest
+
+        if type(model) != str:
+            raise ValueError('model argument must be string type, not {}'.format( str(type(model)) ) )
+        if type(lib) != str:
+            raise ValueError('lib argument must be string type, not {}'.format( str(type(model)) ) )
+        
+        lib = self.findLibraryNameWithAlias(lib)
+        model = self.findModelNameWithAlias(model)
 
         # Get a model object, then instantiate it
         # Then add the model object to our dictionary {id => obj}
-        self.models[ model_id ] = ( self.getObjectFromName(model=model, lib=lib) )(id=model_id, default_args=args)
+        model_obj = self.getObjectFromName(model=model, lib=lib)
+        if model_obj and issubclass( model_obj, Model ):
+            # Need to generate ID for the model and avoid repeats
+            model_id = self.getModelID()
+            while model_id in self.models:
+                model_id = self.getModelID()
+
+            self.models[ model_id ] = model_obj(id=model_id, **args)
         
     # This method will find the specific object we need to use for a given string
     def getObjectFromName(self, model, lib=''):
-        if type(lib) == str:
-            if lib != '':
-                if lib == 'sklearn' or lib == 'scikit-learn':
-                    # It is a scikit-learn model
-                    if model == 'random forest' or model == 'rf':
-                        obj = sklearn.RandomForest
-            else:
-                # We need to try to figure out which one the user wants
-                #  we should output all of the ones we may think match up
-                obj = None
+        obj = None
+        if type(lib) != str:
+            raise ValueError('lib argument must be string type, not {}'.format( str( type(lib) ) ) )
+        if type(model) != str:
+            raise ValueError('model argument must be string type, not {}'.format( str( type(lib) ) ) )
+        
+        if lib != '':
+            if lib == 'scikit-learn':
+                # It is a scikit-learn model
+                if model == 'Random Forest':
+                   obj = sklearn.RandomForest
+        else:
+            # We need to try to figure out which one the user wants
+            #  we should output all of the ones we may think match up
+            obj = None
+            
 
         return obj
 
+    def setDefaultLibrary(self, lib):
+        pass
+
+    def findLibraryNameWithAlias(self, lib=''):
+        if type(lib) == str:
+            if lib:
+                # Go through the alias dictionary to check if the lib exists
+                if lib in self.lib_aliases:
+                    value = self.lib_aliases[ lib ]
+                    
+                    # If the value at this location is a string, then it is the library name
+                    if type(value) == str:
+                        lib = value
+                    elif type(value) == bool: 
+                        if not value:
+                            # This will only occur if we have a library name pointing to False, which is when we
+                            #   are explicitly stating it is not supported
+                            raise ValueError('Library \'{}\' not supported. If you believe this is in error, please create an issue at https://github.com/JohnsonClayton/alexandria/issues'.format(lib))
+                    else:
+                        raise RuntimeError('Illegal state of library alias dictionary: {} => {}'.format( lib, str( value ) ))
+                else:
+                    raise ValueError('Library \'{}\' not found or supported. If you believe this is in error, please create an issue at https://github.com/JohnsonClayton/alexandria/issues'.format(lib))
+            else:
+                # We need to try to figure out which one the user wants
+                #  we should output all of the ones we may think match up
+                warnings.warn('Library unspecified, using default library: \'{}\''.format(self.default_library) )
+                
+                # Default to the default library
+                lib = self.default_library
+
+            return lib
+        else:
+            raise ValueError('Library name must be string type, not {}'.format(  str( type( lib ) )))
+
+
+    def findModelNameWithAlias(self, model):
+        if type(model) == str:
+            if model != '':
+                # Go through the alias dictionary to check if the lib exists
+                if model in self.model_aliases:
+                    value = self.model_aliases[ model ]
+                    
+                    # If the value at this location is a string, then it is the library name
+                    if type(value) == str:
+                        model = value
+                    elif type(value) == bool and not value:
+                        # This will only occur if we have a library name pointing to False, which is when we
+                        #   are explicitly stating it is not supported
+                        raise ValueError('Model \'{}\' not supported. If you believe this is in error, please create an issue at https://github.com/JohnsonClayton/alexandria/issues'.format(model))
+                    else:
+                        raise RuntimeError('Illegal state of model alias dictionary: {} => {}'.format( model, str( value ) ))
+                else:
+                    raise ValueError('alexandria does not recognize model name or alias \'{}\', or this is unsupported in alexandria. If you believe this is in error, please create an issue at https://github.com/JohnsonClayton/alexandria/issues'.format( str(model) ))
+            else:
+                raise ValueError('model argument must be non-empty string!')
+
+            return model
+        else:
+            raise ValueError('Model name must be string type, not {}'.format(  str( type(model) )))
 
--- alexandria/tests/modelsmanager_test.py
+++ alexandria/tests/modelsmanager_test.py
@@ -3,6 +3,8 @@ import unittest
 from alexandria.models import sklearn
 from models import ModelsManager
 
+import warnings
+
 
 def fail(who):
     who.assertTrue( False )
@@ -13,6 +15,11 @@ class TestModelsManager(unittest.TestCase):
         mm = ModelsManager()
 
         self.assertEqual( mm.models, {} )
+
+        # Check that we can set the default library, these tests will be more verbose as
+        #   we implement more libraries
+        mm = ModelsManager(default_library='sklearn')
+        self.assertEqual( mm.default_library, 'scikit-learn' )
 
     def test_addModel(self):
         mm = ModelsManager()
@@ -30,7 +37,7 @@ class TestModelsManager(unittest.TestCase):
         self.assertEqual( mm.models[ expected_id ].getArguments(), arguments )
 
         arguments = {'random_state': 19, 'max_depth': 2}
-        mm.addModel(lib=lib, model=model_name, args=arguments)
+        mm.addModel(lib=lib, model=model_name, default_args=arguments)
         expected_id = 7579
 
         self.assertTrue( expected_id in mm.models )
@@ -38,7 +45,7 @@ class TestModelsManager(unittest.TestCase):
         self.assertEqual( mm.models[ expected_id ].getArguments(), arguments )
         
         arguments = {'random_state': 84, 'max_features': 'sqrt'}
-        mm.addModel(lib=lib, model=model_name, args=arguments)
+        mm.addModel(lib=lib, model=model_name, default_args=arguments)
         expected_id = 4205
         
         self.assertTrue( expected_id in mm.models )
@@ -46,21 +53,166 @@ class TestModelsManager(unittest.TestCase):
         self.assertEqual( mm.models[ expected_id ].getArguments(), arguments )
 
         # Fail if we cannot find a model to add
+        mm = ModelsManager()
+        try:
+            lib = 'unknown library'
+            model = 'random forest'
+            mm.addModel(lib=lib, model=model)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'Library \'{}\' not found or supported. If you believe this is in error, please create an issue at https://github.com/JohnsonClayton/alexandria/issues'.format(lib))
+
+        mm = ModelsManager()
+        try:
+            lib = 'sklearn'
+            model = 'unknown classifier or regressor'
+            mm.addModel(lib=lib, model=model)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'alexandria does not recognize model name or alias \'{}\', or this is unsupported in alexandria. If you believe this is in error, please create an issue at https://github.com/JohnsonClayton/alexandria/issues'.format( model ))
+
+        # Raise a warning when no library is provided
+        print('start warnings')
+        mm = ModelsManager()
+        with warnings.catch_warnings():
+            try:
+                warnings.filterwarnings('error')
+
+                model = 'random forest'
+                mm.addModel( model=model )
+
+                fail(self)
+            except Warning as uw:
+                self.assertEqual( str(uw), 'Library unspecified, using default library: \'scikit-learn\'' )
 
         # Fail if types are incorrect for model, lib, and args
-        fail(self)
+        # Check model type
+        mm = ModelsManager()
+        try:
+            model = 512
+            lib = 'sklearn'
+            args = {'random_state': 0}
+            mm.addModel(model=model, lib=lib, default_args=args)
 
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'model argument must be string type, not {}'.format( str(type(model)) ) )
+
+        mm = ModelsManager()
+        try:
+            model = {'name': 'random forest'}
+            lib = 'sklearn'
+            args = {'random_state': 0}
+            mm.addModel(model=model, lib=lib, default_args=args)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'model argument must be string type, not {}'.format( str(type(model)) ) )
+
+        # Check lib type
+        mm = ModelsManager()
+        try:
+            model = 'random forest'
+            lib = 512
+            args = {'random_state': 0}
+            mm.addModel(model=model, lib=lib, default_args=args)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'lib argument must be string type, not {}'.format( str(type(model)) ) )
+
+        mm = ModelsManager()
+        try:
+            model = 'random forest'
+            lib = ['sklearn', 'scikit-learn']
+            args = {'random_state': 0}
+            mm.addModel(model=model, lib=lib, default_args=args)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'lib argument must be string type, not {}'.format( str(type(model)) ) )
+
+        # Check args type
+        mm = ModelsManager()
+        try:
+            model = 'random forest'
+            lib = 'scikit-learn'
+            args = [{'random_state': 0}]
+            mm.addModel(model=model, lib=lib, default_args=args)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'Default arguments must be dictionary type, not {}'.format( type(args)))
+        mm = ModelsManager()
+        try:
+            model = 'random forest'
+            lib = 'sklearn'
+            args = 512
+            mm.addModel(model=model, lib=lib, default_args=args)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'Default arguments must be dictionary type, not {}'.format( type(args)))
+
+        # TO-DO: Make sure we can add a model hand-crafted by the user
+        # Model object (alexandria object)
+        # ML Library object
+        raise NotImplementedError('Incomplete to-do: Allow hand-crafted models to be added to the models manager')
 
     def test_getObjectFromName(self):
         # Check that all the models can be found using all possible paths
         mm = ModelsManager()
-        self.assertEqual( mm.getObjectFromName(lib='sklearn', model='rf'), sklearn.RandomForest )
-        self.assertEqual( mm.getObjectFromName(lib='scikit-learn', model='random forest'), sklearn.RandomForest )
+        self.assertEqual( mm.getObjectFromName(lib='scikit-learn', model='Random Forest'), sklearn.RandomForest )
 
-        # If library is not provided, then search all the libraries for something with that name
+        # Return None since the lib name has not been aliased
+        mm = ModelsManager()
+        self.assertEqual( mm.getObjectFromName(lib='sklearn', model='Random Forest'), None )
+        self.assertEqual( mm.getObjectFromName(lib='scikit-learn', model='random forest'), None )
 
         # Fail if the library or model arguments are not strings
-        fail(self)
+        mm = ModelsManager()
+        try:
+            lib = 512
+            model = 'random forest'
+            mm.getObjectFromName(lib=lib, model=model)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'lib argument must be string type, not {}'.format( str( type(lib) ) ) )
+
+        mm = ModelsManager()
+        try:
+            lib = [512, 512, 512]
+            model = 'random forest'
+            mm.getObjectFromName(lib=lib, model=model)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'lib argument must be string type, not {}'.format( str( type(lib) ) ) )
+
+        mm = ModelsManager()
+        try:
+            lib = 'sklearn'
+            model = {'random forest':True}
+            mm.getObjectFromName(lib=lib, model=model)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'model argument must be string type, not {}'.format( str( type(lib) ) ) )
+
+        mm = ModelsManager()
+        try:
+            lib = 'scikit-learn'
+            model = ['random forest']
+            mm.getObjectFromName(lib=lib, model=model)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'model argument must be string type, not {}'.format( str( type(lib) ) ) )
+
+    
 
 if __name__ == '__main__':
     unittest.main()
