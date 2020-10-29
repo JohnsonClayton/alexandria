4149f5c4d2b06ae54c2d5774d8e497e78de75017
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2020-10-29 11:50:09 -0600

    added metrics objects

--- alexandria/metrics/metrics.py
+++ alexandria/metrics/metrics.py
@@ -1,3 +1,49 @@
 class Metrics:
     def __init__(self):
-        print('Metric constructor reached!')
+        self.metrics = {}
+
+    def addPair(self, key=None, value=None, keyvalue=None):
+        # If they are providing a dictionary, it should be figured out
+        if type(key) == dict:
+            if len(key) == 1:
+                key, value = list(key.items())[0]
+            elif len(key) > 1:
+                raise ValueError('provided pair contains more than one pairing, please use \'addPairs\' method instead.')
+            else:
+                raise ValueError('provided pair is an empty dictionary!')
+        if type(keyvalue) == dict:
+            if len(keyvalue) == 1:
+                key, value = list(keyvalue.items())[0]
+            elif len(key) > 1:
+                raise ValueError('provided pair contains more than one pairing, please use \'addPairs\' method instead.')
+            else:
+                raise ValueError('provided pair is an empty dictionary!')
+
+        # Check the types of the values and add it to the dictionary
+        if type(key) == str:
+            if type(value) in [str, int, float]:
+                self.metrics[key] = value
+            else:
+                raise ValueError('value must be string, integer, or float type, not {}'.format( str(type(value)) ))
+        else:
+            raise ValueError('key value must be string type, not {}'.format( str( type( key ) ) ))
+
+    def addPairs(self, pairs):
+        if type(pairs) == dict:
+            for key, value in pairs.items():
+                self.addPair(key, value)
+        else:
+            raise ValueError('provided pairs must be dictionary type, not {}'.format( str(type(pairs)) ) )
+
+
+    def getPair(self, key):
+        if type(key) == str:
+            if key in self.metrics:
+                return { key : self.metrics[key] }
+            else:
+                raise ValueError('key value \'{}\' not found'.format( str( type(key) ) ) )
+        else:
+            raise ValueError('key value must be string type, not {}'.format( str(type(key)) ))
+
+    def getPairs(self):
+        return self.metrics
--- alexandria/tests/metrics_test.py
+++ alexandria/tests/metrics_test.py
@@ -9,6 +9,7 @@ class TestMetrics(unittest.TestCase):
     def test_init(self):
         # What would be initialized for a metric value?
         m = Metrics()
+        self.assertEqual( m.metrics, {} )
         
     def test_addPair(self):
         m = Metrics()
@@ -29,6 +30,110 @@ class TestMetrics(unittest.TestCase):
         self.assertEqual( m.getPair('key2'), pair2 )
         self.assertEqual( m.getPair('key3'), pair3 )
         self.assertEqual( m.getPairs(), ultimate_d )
+
+        # Key for the pair cannot be anything but string
+        m = Metrics()
+        try:
+            p1 = {3.45: 512}
+            m.addPair(p1)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'key value must be string type, not {}'.format( str( type( list(p1.keys())[0] ) ) ) )
+
+        m = Metrics()
+        try:
+            p1 = {512: 4.23}
+            m.addPair(p1)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'key value must be string type, not {}'.format( str( type(list(p1.keys())[0]) ) ) )
+
+        # Key does not exist in object failure
+        m = Metrics()
+        m.addPair(pair1)
+        m.addPair(pair2)
+        m.addPair(pair3)
+        try:
+            key4 = 'key4'
+            pair4 = m.getPair(key4)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'key value \'{}\' not found'.format( str( type(key4) ) ) )
+
+
+        # Value for the pair must be string, integer, or real/float types
+        m = Metrics()
+        try:
+            p1 = {'key': [512]}
+            m.addPair(p1)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'value must be string, integer, or float type, not {}'.format( str( type(list(p1.values())[0]) ) ) )
+
+        m = Metrics()
+        try:
+            p1 = {'key': {'key': 512}}
+            m.addPair(p1)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'value must be string, integer, or float type, not {}'.format( str( type(list(p1.values())[0]) ) ) )
+
+        # Fail if provided dictionary with more than one value
+        m = Metrics()
+        try:
+            p1 = {
+                'key1': 'value1',
+                'key2': 'value2'
+            }
+            m.addPair(p1)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'provided pair contains more than one pairing, please use \'addPairs\' method instead.')
+
+        # Fail if provided empty dictionary
+        m = Metrics()
+        try:
+            p1 = {}
+            m.addPair(p1)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'provided pair is an empty dictionary!')
+
+    def test_addPairs(self):
+        # Check for basic functionality
+        m = Metrics()
+
+        pairs = {
+            'key1': 'value1',
+            'key2': 512,
+            'key3': 4.32
+        }
+
+        m.addPairs(pairs)
+        self.assertEqual( m.getPairs(), pairs )
+
+        # If we don't provide a dictionary, then error
+        m = Metrics()
+
+        try:
+            pairs = [{
+                'key1': 'value1',
+                'key2': 512,
+                'key3': 4.32
+            }]
+
+            m.addPairs(pairs)
+
+            fail(self)
+        except ValueError as ve:
+            self.assertEqual( str(ve), 'provided pairs must be dictionary type, not {}'.format( str(type(pairs)) ) )
 
 
 
